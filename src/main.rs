82ebb6cde88e3577ed8650a289a4feacd68bfc3c
noahkrause <nkrausemc@gmail.com>
2020-08-23 17:34:14 -0400

    Unfinished addition of the 'position' command as specified in the universal chess interface standards. Debugging

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use log4rs::{
     Handle,
 };
 use std::io::{self, BufRead};
+use chess::{Board, ChessMove};
+use std::str::{FromStr, SplitWhitespace};
 
 fn get_logger_config(filter: LevelFilter) -> Config {
     let stdout = ConsoleAppender::builder()
@@ -44,6 +46,8 @@ fn main() {
 
     info!("Engine started");
 
+    let mut board: Board = Board::default();
+
     loop {
         let mut line = String::new();
 
@@ -68,9 +72,77 @@ fn main() {
                     },
                     _ => error!("Unrecognized debug option {}", debug_option),
                 }
+            },
+            /*
+             * position [fen <fenstring> | startpos ]  moves <move1> .... <movei>
+             * A FEN string is a chess board notation. Information can be found here:
+             * https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
+             */
+            "position" => {
+                // Grab the next argument in the command. Looking for 'startpos' or 'fen'.
+                let position_option = split.next().unwrap();
+
+                match position_option {
+                    "fen" => {
+                        // Mutable string used to build out the FEN string from the command arguments.
+                        let mut fen_string = "".to_owned();
+
+                        // Loop until there are no more arguments or the 'moves' argument is found.
+                        loop {
+                            let fen_split = split.next();
+
+                            match fen_split {
+                                Some(x) => {
+                                    if x == "moves" {
+                                        board = Board::from_str(&fen_string).expect("Invalid fen string provided!");
+                                        do_moves(&mut board, split);
+                                        break;
+                                    }
+
+                                    if fen_string == "" {
+                                        fen_string = x.to_string();
+                                    } else {
+                                        fen_string = [fen_string, x.to_string()].join(" ");
+                                    }
+                                },
+                                None => board = Board::from_str(&fen_string).expect("Invalid fen string provided!"),
+                            }
+                        }
+                    },
+                    "startpos" => {
+                        board = Board::default();
+                        do_moves(&mut board, split);
+                    }
+                    _ => error!("Unrecognized position option {}", position_option),
+                }
+                println!("Current board position: {}", board);
             }
             "quit" => break,
             _ => info!("Received input: {}", trimmed_line)
         }
     }
+}
+
+fn do_moves(board: &mut Board, mut moves: SplitWhitespace) {
+    let chess_move = moves.next();
+
+    loop {
+        match chess_move {
+            Some(x) => {
+                if x == "moves" {
+                    continue;
+                }
+
+                let mv = ChessMove::from_san(board, x);
+                if mv.is_ok() {
+                    board.make_move(mv.unwrap(), board);
+                } else {
+                    error!("There was an error while attempting to make the chess move: {}", x);
+                }
+            },
+            None => break,
+        }
+    }
+
+    println!("{}", board);
 }
